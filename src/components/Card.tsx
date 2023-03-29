9c041c4fa6b0dd8aa337195470c4c90fe16e6459
William Bradshaw <will.bradshaw50@gmail.com>
2023-03-29 11:26:25 +1100

    cool emoji

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,6 @@ interface props {
 export default function Card(props: props) {
   let card: HTMLDivElement;
   let dialog: HTMLDialogElement;
-  const [open, setOpen] = createSignal(false);
 
   function toggleDialog() {
     if (dialog.open) {
@@ -24,14 +23,14 @@ export default function Card(props: props) {
       <div
         ref={card}
         onClick={toggleDialog}
-        class="bg-white cursor-pointer aspect-square p-5 border border-solid border-gray-200 rounded-xl shadow duration-200 transition-all hover:scale-105 hover:shadow-lg active:shadow-sm active:scale-95"
+        class="bg-white break-words word-wrap hyphens-auto cursor-pointer aspect-square p-5 border border-solid border-gray-200 rounded-xl shadow duration-200 transition-all hover:scale-105 hover:shadow-lg active:shadow-sm active:scale-95"
       >
         <h2 class="text-3xl mb-2">{props.title}</h2>
-        <div class="line-clamp-3">{props.children}</div>
+        <div class="sm:line-clamp-3 line-clamp-5">{props.children}</div>
       </div>
       <dialog
         ref={dialog}
-        class="prose backdrop:bg-gray-200 backdrop:bg-opacity-50 max-w-2xl md:w-3/4 bg-white mx-auto border border-solid border-gray-200 rounded-xl shadow p-5"
+        class="mx-4 prose backdrop:bg-gray-200 backdrop:bg-opacity-50 max-w-2xl md:w-3/4 bg-white md:mx-auto border border-solid border-gray-200 rounded-xl shadow p-5"
       >
         <div class="z-10 text-xs font-mono text-gray-400">{props.title}</div>
         <div class="z-10">{props.children}</div>
